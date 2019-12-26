8e6b9dc3ec0645f8082c4b9ff82df80b271faa4c
SYP <632289574@qq.com>
2019-12-26 15:49:54 +0800

    这是我的第二个git

--- src/com/syp/tt/GitHub.java
+++ src/com/syp/tt/GitHub.java
@@ -2,6 +2,6 @@ package com.syp.tt;
 
 public class GitHub {
     public static void main(String[] args) {
-        System.out.println("打开我的文件的是瓜子");
+        System.out.println("打开我的文件的");
     }
 }
